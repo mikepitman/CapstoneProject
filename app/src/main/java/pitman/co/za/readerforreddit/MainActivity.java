7ea2b5dd8b2cd2e02ac9d7ab2419ffc02126c119
Michael <mike@ubiquitech.co.za>
2018-10-10 10:06:11 +0200

    Task 3: Compile dependencies for Firebase-core added. App linked to Firebase, single FirebaseAnalytics instance created in MainActivity to pass data back to Firebase Dashboard. Todo: add another Firebase / google play utility, possibly authentication. Task 2: Complete Task 1: Complete

--- app/src/main/java/pitman/co/za/readerforreddit/MainActivity.java
+++ app/src/main/java/pitman/co/za/readerforreddit/MainActivity.java
@@ -3,17 +3,24 @@ package pitman.co.za.readerforreddit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 import pitman.co.za.readerforreddit.reddit.QuerySubscribedSubredditsListAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String subreddit1 = "Nokia7Plus";
-    private static String subreddit2 = "techsupport";
+private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         setContentView(R.layout.activity_main);
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.SEARCH_TERM, "Nokia7plus");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SEARCH, bundle);
 
         new QuerySubscribedSubredditsListAsyncTask(this).execute();
     }
